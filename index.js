0a4e6c6f06bcc1a31e7274818680a8220db0f22f
khaled39ab <khaled39ab@gmail.com>
2022-08-19 23:52:17 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
+
+const uri = `mongodb+srv://${process.env.volunteer_user}:${process.env.volunteer_password}@cluster0.eevembd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("volunteerNetwork").collection("member");
+    // perform actions on the collection object
+    console.log('db connected');
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
     res.send('Hello Volunteer network server')
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log("Server running ", port);
 });
